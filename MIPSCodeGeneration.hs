f9beaa624e50116b23d1cd0771f994cc02fea29d
UtsavAch <acharya.utsav.15@gmail.com>
2024-12-09 17:16:41 +0000

    Small refactor

--- MIPSCodeGeneration.hs
+++ MIPSCodeGeneration.hs
@@ -1,67 +1,69 @@
 module MIPSCodeGeneration where
 
 import IR
+import Data.List (find)
+import Data.Maybe (fromMaybe)
+import qualified Data.Map as Map
+
+type StringTable = Map.Map String String
 
 -- Generate MIPS code from a list of IR instructions
 generateMIPS :: [Instr] -> String
 generateMIPS instrs = 
-    ".data\n" ++
-    concatMap generateData instrs ++
-    ".text\n" ++
-    unlines (map convertToMIPS instrs) ++
-    exitProgram
+    let (dataSection, stringTable) = generateData instrs Map.empty
+    in  ".data\n" ++ dataSection ++
+        ".text\n" ++ unlines (map (convertToMIPS stringTable) instrs) ++
+        exitProgram
 
 -- Convert an individual IR instruction to MIPS code
-convertToMIPS :: Instr -> String
-convertToMIPS (MOVE t1 t2) = "move $" ++ t1 ++ ", $" ++ t2
-convertToMIPS (MOVEI t n) = "li $" ++ t ++ ", " ++ show n
-convertToMIPS (MOVES t str) = 
-    "la $a0, " ++ labelName ++ "\n" ++
-    "li $v0, 4\n" ++
-    "syscall"
-  where 
-    labelName = sanitizeString str
-convertToMIPS (ADD t1 t2 t3) = "add $" ++ t1 ++ ", $" ++ t2 ++ ", $" ++ t3
-convertToMIPS (SUB t1 t2 t3) = "sub $" ++ t1 ++ ", $" ++ t2 ++ ", $" ++ t3
-convertToMIPS (MULT t1 t2 t3) = "mul $" ++ t1 ++ ", $" ++ t2 ++ ", $" ++ t3
-convertToMIPS (DIV t1 t2 t3) = "div $" ++ t1 ++ ", $" ++ t2 ++ ", $" ++ t3
-convertToMIPS (MOD t1 t2 t3) = "rem $" ++ t1 ++ ", $" ++ t2 ++ ", $" ++ t3
-convertToMIPS (AND t1 t2 t3) = "and $" ++ t1 ++ ", $" ++ t2 ++ ", $" ++ t3
-convertToMIPS (OR t1 t2 t3) = "or $" ++ t1 ++ ", $" ++ t2 ++ ", $" ++ t3
-convertToMIPS (NOT t1 t2) = "nor $" ++ t1 ++ ", $" ++ t2 ++ ", $zero"
-convertToMIPS (LABEL label) = label ++ ":"
-convertToMIPS (JUMP label) = "j " ++ label
-convertToMIPS (COND t1 binOp t2 label1 label2) = 
-  case binOp of
-    Eq  -> "beq $" ++ t1 ++ ", $" ++ t2 ++ ", " ++ label1 ++ "\nj " ++ label2
-    Neq -> "bne $" ++ t1 ++ ", $" ++ t2 ++ ", " ++ label1 ++ "\nj " ++ label2
-    Lt  -> "blt $" ++ t1 ++ ", $" ++ t2 ++ ", " ++ label1 ++ "\nj " ++ label2
-    LtEq -> "ble $" ++ t1 ++ ", $" ++ t2 ++ ", " ++ label1 ++ "\nj " ++ label2
-    Gt  -> "bgt $" ++ t1 ++ ", $" ++ t2 ++ ", " ++ label1 ++ "\nj " ++ label2
-    GtEq -> "bge $" ++ t1 ++ ", $" ++ t2 ++ ", " ++ label1 ++ "\nj " ++ label2
-convertToMIPS (PRINT t) = 
+convertToMIPS :: StringTable -> Instr -> String
+convertToMIPS stringTable (MOVE t1 t2) = "move $" ++ t1 ++ ", $" ++ t2
+convertToMIPS stringTable (MOVEI t n) = "li $" ++ t ++ ", " ++ show n
+convertToMIPS stringTable (MOVES t str) =
+    let labelName = fromMaybe "" (Map.lookup str stringTable)
+    in "la $a0, " ++ labelName ++ "\n" ++
+       "li $v0, 4\n" ++
+       "syscall"
+convertToMIPS stringTable (ADD t1 t2 t3) = 
+    "add $" ++ t1 ++ ", $" ++ t2 ++ ", $" ++ t3
+convertToMIPS stringTable (SUB t1 t2 t3) = 
+    "sub $" ++ t1 ++ ", $" ++ t2 ++ ", $" ++ t3
+convertToMIPS stringTable (MULT t1 t2 t3) = 
+    "mul $" ++ t1 ++ ", $" ++ t2 ++ ", $" ++ t3
+convertToMIPS stringTable (DIV t1 t2 t3) = 
+    "div $" ++ t1 ++ ", $" ++ t2 ++ ", $" ++ t3
+convertToMIPS stringTable (MOD t1 t2 t3) = 
+    "rem $" ++ t1 ++ ", $" ++ t2 ++ ", $" ++ t3
+convertToMIPS stringTable (LABEL label) = label ++ ":"
+convertToMIPS stringTable (JUMP label) = "j " ++ label
+convertToMIPS stringTable (COND t1 binOp t2 label1 label2) = 
+    case binOp of
+      Eq  -> "beq $" ++ t1 ++ ", $" ++ t2 ++ ", " ++ label1 ++ "\nj " ++ label2
+      Neq -> "bne $" ++ t1 ++ ", $" ++ t2 ++ ", " ++ label1 ++ "\nj " ++ label2
+      Lt  -> "blt $" ++ t1 ++ ", $" ++ t2 ++ ", " ++ label1 ++ "\nj " ++ label2
+      LtEq -> "ble $" ++ t1 ++ ", $" ++ t2 ++ ", " ++ label1 ++ "\nj " ++ label2
+      Gt  -> "bgt $" ++ t1 ++ ", $" ++ t2 ++ ", " ++ label1 ++ "\nj " ++ label2
+      GtEq -> "bge $" ++ t1 ++ ", $" ++ t2 ++ ", " ++ label1 ++ "\nj " ++ label2
+convertToMIPS stringTable (PRINT t) =
     "move $a0, $" ++ t ++ "\n" ++
     "li $v0, 1\n" ++
     "syscall"
-convertToMIPS (PRINTLN t) = 
-    "move $a0, $" ++ t ++ "\n" ++
-    "li $v0, 4\n" ++
-    "syscall"
-convertToMIPS (READ t) = 
+convertToMIPS stringTable (READ t) =
     "li $v0, 5\n" ++
     "syscall\n" ++
     "move $" ++ t ++ ", $v0"
 
--- Generate MIPS code for data section (strings, constants, etc.)
-generateData :: Instr -> String
-generateData (MOVES _ str) = sanitizeString str ++ ": .asciiz \"" ++ str ++ "\"\n"
-generateData _ = ""
-
--- Helper function to sanitize string labels
-sanitizeString :: String -> String
-sanitizeString = map (\c -> if c == ' ' then '_' else c)
+-- Generate MIPS data section and a string table
+generateData :: [Instr] -> StringTable -> (String, StringTable)
+generateData [] table = ("", table)
+generateData (MOVES _ str : rest) table =
+    let labelName = fromMaybe ("str" ++ show (Map.size table)) (Map.lookup str table)
+        newTable = Map.insert str labelName table
+        dataSection = labelName ++ ": .asciiz \"" ++ str ++ "\"\n"
+        (restData, finalTable) = generateData rest newTable
+    in (dataSection ++ restData, finalTable)
+generateData (_ : rest) table = generateData rest table
 
 -- Add a syscall for program termination
 exitProgram :: String
-exitProgram = 
-    "li $v0, 10\nsyscall\n"
+exitProgram = "li $v0, 10\nsyscall\n"
--- tests/example8.kt
+++ tests/example8.kt
@@ -1,14 +1,10 @@
 fun main() {
     print("Enter a number")
-    // var input: Int = readln() -- Need to fix this issue in semantics
     var factorial: Int = 1
     var i: Int = 1
 
-    // while (i <= input) {
     while (i <= 10) {
         factorial *= i
         i++
     }
-
-    print("Factorial is some num")
 }
